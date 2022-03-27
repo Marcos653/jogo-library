9cfd30f792653b06221dfb46ec93b68629c5353a
Marcos653 <viniciustatuta@gmail.com>
2022-03-27 17:45:52 -0300

    base64

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,7 +1,9 @@
 from core.models import Jogo
 from rest_framework.serializers import ModelSerializer
+from drf_base64.fields import Base64ImageField, Base64FileField
 
 class JogoSerializer(ModelSerializer):
+    image = Base64ImageField(required=False)
     class Meta:
         model = Jogo
         fields = '__all__' 
